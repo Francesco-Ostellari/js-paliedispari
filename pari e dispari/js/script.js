e8a255fc115e5656d8a098a264ced2ac3aade645
Francesco-Ostellari <francescoostellari@gmail.com>
2021-11-29 16:25:02 +0100

    add-metodo-funzione

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -10,6 +10,50 @@
 // metodo senza funzioni
 
 // faccio scegliere all'utente se preferisce pari o dispari
+// let scelta = prompt ('preferisci pari o dispari?');
+
+// faccio inserire all'utente un numero tra 1 e 5
+// let numUser = parseInt(prompt ('insersci un numero da 1 a 5'));
+
+// dichiaro la variabile sum che mi servirà per fare la somma dei due numeri
+// let sum ='';
+
+// stampo la scelta pari o dispari
+// console.log ('hai scelto: ' + scelta);
+
+// stampo il numero scelto dall'utente
+// console.log ("numero scelto dall'utente: " + numUser);
+
+// dichiaro e uso la funzione per generare numeri random da 1 a 5
+// let numPc = parseInt((Math.floor(Math.random() * 5) + 1));
+
+// stampo il numero random
+// console.log('numero scelto dal pc: ', numPc);
+
+// sommo il numero random con il numero scelto dall'utente
+// sum = numUser + numPc;
+
+// stampo la somma
+// console.log('la somma dei due numeri è: ' + sum);
+
+// se la somma è pari allora
+// if (sum % 2==0) {
+//   console.log('il numero sommato è pari');
+// } else { // altrimenti
+//   console.log('il numero sommato è dispari');
+// }
+
+// se la somma è pari e l'utente ha scelto pari o se la somma è dispari e l'utente ha scelto dispari allora
+// if (sum % 2 == 0 && scelta == "pari" || sum % 2 != 0 && scelta == "dispari") {
+//   console.log("Ha vinto l'utente");
+// } else { //altrimenti
+//   console.log('Ha vinto il PC');
+// }
+
+// metodo con funzioni
+
+
+// faccio scegliere all'utente se preferisce pari o dispari
 let scelta = prompt ('preferisci pari o dispari?');
 
 // faccio inserire all'utente un numero tra 1 e 5
@@ -21,31 +65,41 @@ let sum ='';
 // stampo la scelta pari o dispari
 console.log ('hai scelto: ' + scelta);
 
-// stampo il numero scelto dall'utente
+// // stampo il numero scelto dall'utente
 console.log ("numero scelto dall'utente: " + numUser);
 
 // dichiaro e uso la funzione per generare numeri random da 1 a 5
-let numPc = parseInt((Math.floor(Math.random() * 5) + 1));
+
+function randNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let numPc = randNum(1, 5);
 
 // stampo il numero random
 console.log('numero scelto dal pc: ', numPc);
 
 // sommo il numero random con il numero scelto dall'utente
+
 sum = numUser + numPc;
 
 // stampo la somma
 console.log('la somma dei due numeri è: ' + sum);
 
 // se la somma è pari allora
-if (sum % 2==0) {
-  console.log('il numero sommato è pari');
-} else { // altrimenti
-  console.log('il numero sommato è dispari');
+function isEven(num) {
+  if (num % 2 == 0) {
+    console.log('il numero sommato è pari');
+    return true;
+  } else { //altrimenti
+    console.log('il numero sommato è dispari');
+    return false;
+  }
 }
 
 // se la somma è pari e l'utente ha scelto pari o se la somma è dispari e l'utente ha scelto dispari allora
-if (sum % 2 == 0 && scelta == "pari" || sum % 2 != 0 && scelta == "dispari") {
-  console.log("Ha vinto l'utente");
-} else { //altrimenti
-  console.log('Ha vinto il PC');
+
+if (isEven(sum) == true && scelta == "pari" || isEven(sum) == false && scelta == "dispari") {
+  console.log("ha vinto l'utente");
+} else {
+  console.log('ha vinto il PC');
 }
