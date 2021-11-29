30a9387d2e642fe54c1ad59432f0ae67985e949d
Francesco-Ostellari <francescoostellari@gmail.com>
2021-11-29 15:04:35 +0100

    metodo-con-funzione

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,22 +4,44 @@ Creare una funzione per capire se la parola inserita è palindroma
   (prima senza funzione e poi con funzione) */
 
 
+// metodo senza funzione
 // chiedo all'utente una parola
 let parolaUser = prompt ('inserisci una parola');
 // dichiaro la variabile della parola scritta al contrario
 let parolaContrario = '';
 
 // ciclo for per trovare la parola al contrario
-for (let index = parolaUser.length - 1; index>=0; index--) {
-  parolaContrario += parolaUser[index]; 
+// for (let index = parolaUser.length - 1; index>=0; index--) {
+//   parolaContrario += parolaUser[index]; 
+// }
+
+// // stampo le due parole
+// console.log(parolaUser);
+// console.log(parolaContrario);
+
+// // se la parola dell'utente è uguale alla parola scritta al contrario allora
+// if (parolaUser == parolaContrario) {
+//   console.log('la parola è palindroma');
+// } else { // altrimenti
+//   console.log('la parola non è palindroma');
+// }
+
+// metodo con funzione
+
+function reverseWord(parolaUser) {
+  let reverseString = "";
+  for (let index = parolaUser.length - 1; index >= 0; index--) {
+    reverseString += parolaUser[index];
+  }
+  return reverseString;
 }
 
-// stampo le due parole
 console.log(parolaUser);
-console.log(parolaContrario);
+console.log(reverseWord(parolaUser));
 
-// se la parola dell'utente è uguale alla parola scritta al contrario allora
-if (parolaUser == parolaContrario) {
+
+// // se la parola dell'utente è uguale alla parola scritta al contrario allora
+if (parolaUser == reverseWord(parolaUser)) {
   console.log('la parola è palindroma');
 } else { // altrimenti
   console.log('la parola non è palindroma');
